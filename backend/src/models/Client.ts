3244e4597b483fa5d093c8d70daee2e6acba3715
joaquinq <joacoquintero64@gmail.com>
2024-12-11 21:15:02 -0500

    Creación del modelo cliente de la base de datos

---
+++ backend/src/models/Client.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IClient extends Document {
+  name: string;
+  email: string;
+  address: string;
+  city: string;
+  tel_numb: string;
+  reg_date: Date;
+}
+
+const ClientSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  tel_numb: { type: String, required: true },
+  reg_date: { type: Date, default: Date.now },
+});
+
+export default mongoose.model<IClient>("Clients", ClientSchema);
