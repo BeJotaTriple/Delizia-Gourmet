9cf247df79118067e91130ad0aaaa1e83cdce487
joaquinq <joacoquintero64@gmail.com>
2024-12-11 21:22:44 -0500

    Creación del modelo producto de la base de datos

---
+++ backend/src/models/Product.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IProduct extends Document {
+  name: string;
+  description: string;
+  ingredients: string;
+  category: mongoose.Types.ObjectId;
+  stock: number;
+  price: number;
+}
+
+const ProductSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  ingredients: { type: String, required: true },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Categories",
+    required: true,
+  },
+  stock: { type: Number, required: true, min: 0 },
+  price: { type: Number, required: true, min: 0 },
+});
+
+export default mongoose.model<IProduct>("Products", ProductSchema);
