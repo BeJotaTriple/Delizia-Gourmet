509d0c9bd5f49860fa76498d012a1226b3ba8846
J Daniel Soto C <joseda.sotoca@gmail.com>
2024-12-16 01:42:09 -0500

    Navegación a ruta no encontrada

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,18 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
-import Categorias from './components/Categories'
-import AddProduct from './components/AddProduct'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Categorias from './components/Categories';
+import AddProduct from './components/AddProduct';
 import EditProduct from './components/EditProduct';
-import Clients from './components/Clients'
-import Products from './components/Products'
-import AddClient from './components/AddClient'
+import Clients from './components/Clients';
+import Products from './components/Products';
+import AddClient from './components/AddClient';
 import EditClient from "./components/EditClient";
 import AddCategory from './components/AddCategory';
 import EditCategory from './components/EditCategory';
-import Footer from './components/Footer'
-import Dashboard from './components/Dashboard'
+import Footer from './components/Footer';
+import Dashboard from './components/Dashboard';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -32,7 +33,7 @@ function App() {
         <Route path="/edit-client/:id" element={<EditClient />} />
         <Route path='/products' element={<Products />} />
         <Route path="/edit-product/:id" element={<EditProduct />} />
-        <Route path="*" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer/>
     </Router>
