5efbf75d60fe739a0643df634c53aae634b32a2a
J Daniel Soto C <joseda.sotoca@gmail.com>
2024-12-16 02:32:00 -0500

    Ajuste de pagina 404

--- frontend/src/components/NotFound.tsx
+++ frontend/src/components/NotFound.tsx
@@ -1,16 +1,18 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import questionIcon from "../assets/QuestionIcon.svg"
 
 const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-red-400 mb-4">
+      <img src={questionIcon} alt="Logo" className=" max-w-sm max-h-sm text-slate-500" />
+
+      <h1 className="text-4xl font-bold text-red-600 mb-4">
         Página no encontrada
       </h1>
       <p className="text-lg text-gray-700 mb-6">
         Lo sentimos, no pudimos encontrar la página que estás buscando.
       </p>
-      <Link to="/" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+      <Link to="/" className="px-4 py-2 bg-blue-600 text-white font-bold rounded hover:bg-gray-700 hover:text-white">
         Volver a la página principal
       </Link>
     </div>
